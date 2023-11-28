778bfe4c129790cf59afa2de8684278ae24bf4fe
huyen <claire.ngn22@gmail.com>
2023-11-28 12:51:37 +0100

    version 2 dtb

--- Team-builder.sql
+++ Team-builder.sql
@@ -5,43 +5,66 @@
 USE [team-builder]
 CREATE TABLE [personne] (
   [id] int PRIMARY KEY IDENTITY(1, 1),
-  [team_id] int,
-  [role] varchar(15),
-  [prenom] varchar(15),
-  [nom] varchar(15)
+  [nom] varchar(255) NOT NULL,
+  [prenom] varchar(255) NOT NULL
 )
 GO
 
-CREATE TABLE [team] (
+CREATE TABLE [equipe] (
   [id] int PRIMARY KEY IDENTITY(1, 1),
-  [nom] varchar(10),
-  [role] varchar(50)
+  [nom] varchar(255) NOT NULL,
+  [projet] varchar(255) NOT NULL,
+  [personne_id] int
 )
 GO
 
-ALTER TABLE [personne] ADD FOREIGN KEY ([team_id]) REFERENCES [team] ([id])
+CREATE TABLE [equipe_has_personne] (
+  [personne_id] int,
+  [equipe_id] int,
+  PRIMARY KEY ([personne_id], [equipe_id])
+)
+GO
+
+ALTER TABLE [equipe] ADD FOREIGN KEY ([personne_id]) REFERENCES [personne] ([id])
+GO
+ALTER TABLE [equipe_has_personne] ADD FOREIGN KEY ([equipe_id]) REFERENCES [equipe] ([id])
+GO
+ALTER TABLE [equipe_has_personne] ADD FOREIGN KEY ([personne_id]) REFERENCES [personne] ([id])
 GO
 /*
-INSERT INTO [team] ([nom],[role]) VALUES ('Team A','Projet site internet Mairie')
-INSERT INTO [team] ([nom],[role]) VALUES ('Team B','Projet CRM')
-INSERT INTO [team] ([nom],[role]) VALUES ('Team C','Projet ERP')
+INSERT INTO [equipe] ([nom],[projet],[personne_id]) VALUES ('Team A','Projet site internet Mairie',1)
+INSERT INTO [equipe] ([nom],[projet],[personne_id]) VALUES ('Team B','Projet CRM',4)
+INSERT INTO [equipe] ([nom],[projet],[personne_id]) VALUES ('Team C','Projet ERP',7)
 
-INSERT INTO [personne] ([team_id],[role],[prenom],[nom]) VALUES (1,'chef','Brad','Pitt')
-INSERT INTO [personne] ([team_id],[role],[prenom],[nom]) VALUES (1,'designer','Bruce','Willis')
-INSERT INTO [personne] ([team_id],[role],[prenom],[nom]) VALUES (1,'designer','Nicolas','Cage')
+INSERT INTO [personne] ([prenom],[nom]) VALUES ('Brad','Pitt')
+INSERT INTO [personne] ([prenom],[nom]) VALUES ('Bruce','Willis')
+INSERT INTO [personne] ([prenom],[nom]) VALUES ('Nicolas','Cage')
 
-INSERT INTO [personne] ([team_id],[role],[prenom],[nom]) VALUES (2,'chef','Angelina','Jolie')
-INSERT INTO [personne] ([team_id],[role],[prenom],[nom]) VALUES (2,'designer','Tom','Cruise')
-INSERT INTO [personne] ([team_id],[role],[prenom],[nom]) VALUES (2,'designer','Tom','Hanks')
+INSERT INTO [personne] ([prenom],[nom]) VALUES ('Angelina','Jolie')
+INSERT INTO [personne] ([prenom],[nom]) VALUES ('Tom','Cruise')
+INSERT INTO [personne] ([prenom],[nom]) VALUES ('Tom','Hanks')
 
-INSERT INTO [personne] ([team_id],[role],[prenom],[nom]) VALUES (3,'chef','Bob','Jolie')
-INSERT INTO [personne] ([team_id],[role],[prenom],[nom]) VALUES (3,'designer','Johnny','Cash')
-INSERT INTO [personne] ([team_id],[role],[prenom],[nom]) VALUES (3,'designer','Jimmy','Hendrix')
+INSERT INTO [personne] ([prenom],[nom]) VALUES ('Bob','Jolie')
+INSERT INTO [personne] ([prenom],[nom]) VALUES ('Johnny','Cash')
+INSERT INTO [personne] ([prenom],[nom]) VALUES ('Jimmy','Hendrix')
+
+INSERT INTO [equipe_has_personne] ([personne_id],[equipe_id]) VALUES (1,1)
+INSERT INTO [equipe_has_personne] ([personne_id],[equipe_id]) VALUES (2,1)
+INSERT INTO [equipe_has_personne] ([personne_id],[equipe_id]) VALUES (3,1)
+
+INSERT INTO [equipe_has_personne] ([personne_id],[equipe_id]) VALUES (4,2)
+INSERT INTO [equipe_has_personne] ([personne_id],[equipe_id]) VALUES (5,2)
+INSERT INTO [equipe_has_personne] ([personne_id],[equipe_id]) VALUES (6,2)
+
+INSERT INTO [equipe_has_personne] ([personne_id],[equipe_id]) VALUES (7,3)
+INSERT INTO [equipe_has_personne] ([personne_id],[equipe_id]) VALUES (8,3)
+INSERT INTO [equipe_has_personne] ([personne_id],[equipe_id]) VALUES (9,3)
 */
-SELECT * FROM [personne] INNER JOIN [team] 
-ON [personne].[team_id] = [team].[id]
-WHERE [team_id] = 1
+SELECT p.* FROM personne p
+JOIN equipe_has_personne ep ON p.id = ep.personne_id
+WHERE equipe_id = 1
 
-SELECT * FROM [team] INNER JOIN [personne]
-ON [team].[id] = [personne].[team_id]
-ORDER BY [team_id] ASC
+SELECT p.*
+FROM personne p
+JOIN equipe_has_personne ep ON p.id = ep.personne_id
+ORDER BY ep.equipe_id
